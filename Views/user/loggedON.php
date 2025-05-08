f5c9f9c58127ddff4c9d07d831277b3412f37d1c
Ivo <ivospada03@gmail.com>
2025-05-08 12:55:20 -0300

    siguiendo flujo

--- Views/user/loggedON.php
+++ Views/user/loggedON.php
@@ -22,14 +22,14 @@ $id_usuario = $_SESSION['id_usuario'];
 <body>
     <h1>Bienvenido</h1>
     <p>Has iniciado sesión exitosamente.</p>
-    <!-- <h1>Hola, <?php echo htmlspecialchars($nombre_usuario); ?>!</h1>
-    <p>Tu ID de usuario es: <?php echo htmlspecialchars($id_usuario); ?></p> -->
+    <h1>Hola, <?php echo htmlspecialchars($_SESSION['nombre'] . " " . $_SESSION['apellido']); ?></h1>
+    <p>Tu ID de usuario es: <?php echo htmlspecialchars($id_usuario); ?></p>
     <p>Tu peso es: <?php echo $_SESSION['peso']; ?> kg</p>
     <p>Tu altura es: <?php echo $_SESSION['altura']; ?> cm</p>
     <p>Tu IMC es: <?php echo $_SESSION['imc']; ?></p>
     <p>Tu objetivo calórico: <?php echo $_SESSION['obj_kcal']; ?> kcal</p>
     <p>Tu objetivo físico: <?php echo $_SESSION['obj_fisico']; ?></p>
-    <a href="cerrarsesion.php">Cerrar sesión</a>
+    <a href="../../assets/php/logged-resources/cerrarsesion.php">Cerrar sesión</a>
     <a href="data-register.html">Ingresar resto de datos</a>
 </body>
 </html>
--- assets/php/logged-resources/obtenerDatos.php
+++ assets/php/logged-resources/obtenerDatos.php
@@ -14,7 +14,7 @@ if ($conn->connect_error) {
 
 $id_usuario = $_SESSION['id_usuario'];
 
-$sql = "SELECT peso, altura, imc, obj_kcal, obj_fisico FROM usuario WHERE id_usuario = $id_usuario";
+$sql = "SELECT peso, altura, imc, obj_kcal, obj_fisico, nombre, apellido FROM usuario WHERE id_usuario = $id_usuario";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
@@ -25,6 +25,8 @@ if ($result->num_rows > 0) {
     $_SESSION['imc'] = $datos['imc'];
     $_SESSION['obj_kcal'] = $datos['obj_kcal'];
     $_SESSION['obj_fisico'] = $datos['obj_fisico'];
+    $_SESSION['nombre'] = $datos['nombre'];
+    $_SESSION['apellido'] = $datos['apellido'];
 
 } else {
     echo "Usuario no encontrado.";
